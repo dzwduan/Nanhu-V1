f7aa9461bbed28351ca8207821b1dd5bb22b2e86
dzwduan <dzwduan@gmail.com>
2024-08-10 13:37:57 +0800

    add MEFreeList from xs

---
+++ src/main/scala/nanhu/rename/MEFreeList.scala
@@ -0,0 +1,74 @@
+/***************************************************************************************
+* Copyright (c) 2020-2021 Institute of Computing Technology, Chinese Academy of Sciences
+* Copyright (c) 2020-2021 Peng Cheng Laboratory
+*
+* XiangShan is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
+
+package nanhu.rename
+
+
+import chisel3._
+import chisel3.util._
+import nanhu.utils._
+import nanhu._
+
+
+class MEFreeList(size: Int)(implicit p: Parameter) extends BaseFreeList(size) {
+  val freeList = RegInit(VecInit(
+    // originally {1, 2, ..., size - 1} are free. Register 0-31 are mapped to x0.
+    Seq.tabulate(size - 1)(i => (i + 1).U(PhyRegIdxWidth.W)) :+ 0.U(PhyRegIdxWidth.W)))
+
+  // head and tail pointer
+  val headPtr = RegInit(FreeListPtr(false, 0))
+  val headPtrOH = RegInit(1.U(size.W))
+  
+  val headPtrOHShift = CircularShift(headPtrOH)
+  // may shift [0, RenameWidth] steps
+  val headPtrOHVec = VecInit.tabulate(RenameWidth + 1)(headPtrOHShift.left)
+  val tailPtr = RegInit(FreeListPtr(false, size - 1))
+
+  val doRename = io.canAllocate && io.doAllocate && !io.redirect && !io.walk
+
+  /**
+    * Allocation: from freelist (same as StdFreelist)
+    */
+  val phyRegCandidates = VecInit(headPtrOHVec.map(sel => Mux1H(sel, freeList)))
+  for (i <- 0 until RenameWidth) {
+    // enqueue instr, is move elimination
+    io.allocatePhyReg(i) := phyRegCandidates(PopCount(io.allocateReq.take(i)))
+  }
+  // update head pointer
+  val numAllocate = PopCount(io.allocateReq)
+  val headPtrNext = headPtr + numAllocate
+  headPtr := Mux(doRename, headPtrNext, headPtr)
+  headPtrOH := Mux(doRename, headPtrOHVec(numAllocate), headPtrOH)
+
+
+  /**
+    * Deallocation: when refCounter becomes zero, the register can be released to freelist
+    */
+  for (i <- 0 until CommitWidth) {
+    when (io.freeReq(i)) {
+      val freePtr = tailPtr + PopCount(io.freeReq.take(i))
+      freeList(freePtr.value) := io.freePhyReg(i)
+    }
+  }
+
+  // update tail pointer
+  val tailPtrNext = tailPtr + PopCount(io.freeReq)
+  tailPtr := tailPtrNext
+
+  val freeRegCnt = Mux(doRename, distanceBetween(tailPtrNext, headPtrNext), distanceBetween(tailPtrNext, headPtr))
+  val freeRegCntReg = RegNext(freeRegCnt)
+  io.canAllocate := freeRegCntReg >= RenameWidth.U
+}
